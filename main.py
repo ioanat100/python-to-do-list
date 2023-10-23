36b67b4e1d48474dc0ad26c5373b0f5e9f081faa
ioanat100 <ioana_t100@outlook.com>
2023-10-23 16:40:55 +0300

    change edit case to match ennumeration to selection

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 todos = []
 
 while True:
-    user_action=input("Type add, show, edit or exit: ")
-    user_action=user_action.strip()
+    user_action = input("Type add, show, edit or exit: ")
+    user_action = user_action.strip()
 
     match user_action:
         case 'add':
-            todo=input("Enter a to-do: ")
+            todo = input("Enter a to-do: ")
             todos.append(todo)
         case 'show':
-            for item in todos:
-                print(item)
+            for index, item in enumerate(todos):
+                print(index, item)
         case 'edit':
-            number = int(input("Number of the to-do to edit: "))-1
-            existing_todo=todos[number]
+            number = int(input("Number of the to-do to edit: "))
+            existing_todo = todos[number]
             print("You will edit: ", existing_todo)
-            new_todo=input("new to-do: ")
-            todos[number]=new_todo
+            new_todo = input("new to-do: ")
+            todos[number] = new_todo
         case 'exit':
             break
 
