f5999c63de5e2e85235edb76d1f8c5b534a80bf6
ioanat100 <ioana_t100@outlook.com>
2023-11-06 14:13:25 +0200

    replace match-case with if-elif-else across all options and within add option. add option to add in one line. start using with context statements

--- main.py
+++ main.py
@@ -4,35 +4,36 @@ while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
-    match user_action:
-        case 'add':
+    if user_action.startswith('add'):
+        if len(user_action) > 4:
+            todo = user_action[4:] + '\n'
+        else:
             todo = input("Enter a to-do: ") + "\n"
-
-            file = open('todo.txt', 'r')
+        with open('todo.txt', 'r') as file:
             todos = file.readlines()
-            file.close()
             todos.append(todo)
-            file = open('todo.txt','w')
+        with open('todo.txt', 'w') as file:
             file.writelines(todos)
-            file.close()
-        case 'show':
-            file = open('todo.txt','r')
-            print(file.read())
-            file.close()
-        case 'edit':
-            number = int(input("Number of the to-do to edit: "))
-            existing_todo = todos[number]
-            print("You will edit: ", existing_todo)
-            new_todo = input("new to-do: ")
-            todos[number] = new_todo
-        case 'complete':
-            number = int(input("Number of the completed to-do: "))
-            print("You will remove ", todos[number], "from the list.")
-            confirm = input("Proceed? (y/n)")
-            match confirm:
-                case "y" | "yes" | "1":
-                    todos.pop(number)
-        case 'exit':
-            break
-
+    elif user_action.startswith('show'):
+        file = open('todo.txt', 'r')
+        print(file.read())
+        file.close()
+    elif user_action.startswith('edit'):
+        number = int(input("Number of the to-do to edit: "))
+        existing_todo = todos[number]
+        print("You will edit: ", existing_todo)
+        new_todo = input("new to-do: ")
+        todos[number] = new_todo
+    elif user_action.startswith('complete'):
+        number = int(input("Number of the completed to-do: "))
+        print("You will remove ", todos[number], "from the list.")
+        confirm = input("Proceed? (y/n)")
+        match confirm:
+            case "y" | "yes" | "1":
+                todos.pop(number)
+    elif user_action.startswith('exit'):
+        break
+    else:
+        print("You entered an unexpected command. No action was taken.")
+        continue
 print("Bye!")
